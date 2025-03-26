2d0173e8776d2406f89e4ad6ff8c618fc86f0adf
Jarcek <jaromirhalamasek@seznam.cz>
2025-03-26 20:24:08 +0100

    nic zajimaveho

--- klikacka/script.js
+++ klikacka/script.js
@@ -23,5 +23,30 @@ function toggleSidebar() {
     open_btn.style.display = "none"; // Hide ☰ button
   }
 }
+function barva(barva){
+  let Sbarva;
+  switch(barva){
+    case "blueviolet":
+      Sbarva = "#333"
+      break
+    case "goldenrod":
+      Sbarva = "#333"
+      break
+    default:
+      Sbarva = barva;
+      break
 
+  }
+  //to pod neni treba protoze ta default barva je peak
+  // document.querySelector('.stavova_cast').style.backgroundColor = Sbarva;
+  document.querySelector('.klikaci_cast').style.backgroundColor = barva;
+  document.querySelector('.pocty').style.backgroundColor = barva;
+  document.querySelector('.nakupni_cast').style.backgroundColor = barva;
+  
+}
+const toggleButton = document.getElementById('theme-toggle');
+
+toggleButton.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+});
 updateDisplay()
